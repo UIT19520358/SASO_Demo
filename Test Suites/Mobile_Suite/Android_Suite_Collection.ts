5d8038cae78e0ad6605e8f2851164d742cceca9b
vaibhavjain <vaibhavjain@192.168.225.40>
2024-02-01 13:06:05 +0530

    Commit Collection changes

--- Test Suites/Mobile_Suite/Android_Suite_Collection.ts
+++ Test Suites/Mobile_Suite/Android_Suite_Collection.ts
@@ -27,5 +27,15 @@
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/Mobile_Suite/Mobile_Auto_Suite</testSuiteEntity>
       </TestSuiteRunConfiguration>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/Mobile_Suite/Mobile_Auto_Suite</testSuiteEntity>
+      </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>

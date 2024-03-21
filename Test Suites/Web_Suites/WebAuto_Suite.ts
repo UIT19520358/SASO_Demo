729316152f22b1b609265c9a90b022e32faffa91
vaibhavjain <vaibhavjain@localhost>
2024-03-21 19:13:32 +0530

    Commit_Push

--- Test Suites/Web_Suites/WebAuto_Suite.ts
+++ Test Suites/Web_Suites/WebAuto_Suite.ts
@@ -56,47 +56,47 @@
       </variableLink>
    </testCaseLink>
    <testCaseLink>
-      <guid>ac8c7cb4-1f41-4bf6-8d62-6476f72e1fd2</guid>
+      <guid>6a7a5419-8b39-4a3b-b3b1-cbc75aa380a0</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Web_Automation/Web_Automation_TC1</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>0bc70ad8-a408-4c9d-a5b7-2f6e1248a6a9</guid>
+      <guid>d5df7eb4-b3dd-44a8-a793-88c2a609f81d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/API_Automation/Find user by ID</testCaseId>
+      <testCaseId>Test Cases/API_Automation/Create a new user</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
          <testDataLinkId></testDataLinkId>
          <type>DEFAULT</type>
          <value></value>
-         <variableId>cdbfb918-23e8-47c9-98a0-94180ea738a8</variableId>
+         <variableId>cd6d02c1-1f14-45a0-85c0-6fdc8aa540c1</variableId>
       </variableLink>
       <variableLink>
          <testDataLinkId></testDataLinkId>
          <type>DEFAULT</type>
          <value></value>
-         <variableId>3c18d2e3-1084-4cd8-8aee-7a7b987b32a5</variableId>
+         <variableId>a290b7b9-e616-4850-8ddd-0578ca48a053</variableId>
       </variableLink>
       <variableLink>
          <testDataLinkId></testDataLinkId>
          <type>DEFAULT</type>
          <value></value>
-         <variableId>59c707fd-5482-473b-aafb-0eb899791c6e</variableId>
+         <variableId>9f172c61-4aab-4ad3-a41c-d6673ec7c2b9</variableId>
       </variableLink>
       <variableLink>
          <testDataLinkId></testDataLinkId>
          <type>DEFAULT</type>
          <value></value>
-         <variableId>aa163211-755f-4f4e-8e17-76547995f8f5</variableId>
+         <variableId>32e7650a-29d8-4f55-adaa-ea6fa8c784ec</variableId>
       </variableLink>
       <variableLink>
          <testDataLinkId></testDataLinkId>
          <type>DEFAULT</type>
          <value></value>
-         <variableId>932223f6-a8ad-438e-8faa-194d6f8b670e</variableId>
+         <variableId>ab23fe32-540f-466a-843f-a5498904c7be</variableId>
       </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
